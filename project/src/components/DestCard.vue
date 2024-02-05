47c90156ff0646bdcd5f0898d5e6b8da9ce85239
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-02-05 13:11:55 -0500

    ;lkdsjgfphb

---
+++ project/src/components/DestCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+<h2>{{ Destination.name }}</h2>
+<img :src="Destination.img" alt="">
+<button @click="increment">Click me</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+    const props = defineProps({
+        Destination: Object
+    });
+    
+    //clicker logic
+    const clicked = ref(0);
+    function increment(){
+        clicked.value++;
+    }
+</script>
+
+<style scoped>
+img {
+    width: 300px;
+
+}
+</style>
