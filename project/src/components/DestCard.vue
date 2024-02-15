0ce3877c1cb6845ced79eb60ac6155daaa9159e3
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-02-15 13:29:29 -0500

    printed the list

--- project/src/components/DestCard.vue
+++ project/src/components/DestCard.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-    <li v-for="color in colors" :key="colors.id" >
-      <input type ="checkbox">
-    <span :class="colortitles">{{ color.name }}</span>
-    </li>
+    <ul  :id="list" v-for="color in colors" :key="colors.id" >
+    <button :class="button">{{ color.name }}</button>
+    </ul>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+let id = 0;
 const colors = ref([
-  {id: id++, name: White}
+  {id: id++, name: "White"},
+  {id: id++, name: "Black"},
+  {id: id++, name: "Guards Red"},
+  {id: id++, name: "Racing Yellow"},
+  {id: id++, name: "GT Silver Metallic"},
+  {id: id++, name: "Arctic Grey"},
+  {id: id++, name: "Shark Blue"},
+  {id: id++, name: "Ice Grey Metallic"},
 ])
 </script>
 
@@ -20,5 +27,13 @@ img {
   width: 300px;
   height: 350px;
   object-fit: cover;
+}
+button {
+  width: 7rem;
+  height: 3rem;
+}
+#list {
+  display:flex;
+  flex-direction: row;
 }
 </style>
--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -9,13 +9,6 @@
 import DestCard from "@/components/DestCard.vue";
 import { ref } from "vue";
 
-let id = 0
-
-const cars = ref([
-  { id: id++, img: "https://shorturl.at/ajKS5" },
-  { id: id++, img: "https://shorturl.at/ehlt9" },
-])
-
 </script>
 
 <style scoped></style>
